f1db92dce9473f832011cb48cb21a4c54d2b4302
Abbas212 <aviranimahdiabbas212@gmail.com>
2021-04-28 02:24:08 +0530

    Comments Added

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,12 @@
+## This pair of functions allows you to store the inverse of a matrix so
+## it can be called in the future without having to be recalculated
+
+
+## makeCacheMatrix initializes the function argument as x,
+##  sets the value of m to NULL and defines four other functions
+## the four other functions are stored in a named list so they can easily be retrieved
+
+
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
@@ -9,6 +18,9 @@ makeCacheMatrix <- function(x = matrix()) {
         getinv <- function() {inv}
         list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
+
+## cacheSolve checks to see if there is a stored value of m, the inverse matrix
+## if there is it returns it, if it is NULL it calculates it, stores it, and then prints it
 
 cacheSolve <- function(x, ...) {
         inv <- x$getinv()
